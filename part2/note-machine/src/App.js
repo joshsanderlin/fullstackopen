b6abd4c19029ae2fbc22dad1de9a3e7c182c1d14
Josh Sanderlin <josh.sanderlin@elasticsuite.com>
2020-11-22 16:28:28 -0700

    Finished Part 3c

--- part2/note-machine/src/App.js
+++ part2/note-machine/src/App.js
@@ -65,7 +65,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes for days</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
